9408a138e8e3e4af8c737b63bca2aeef76522163
Javier <jamg44@gmail.com>
2023-06-17 16:03:14 +0200

    enable nwjs in vue components

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import applyPrototypes from '@/helpers/applyPrototypes.js';
 import App from '@/App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// Make NW.js and Node globals available in Vue
+applyPrototypes(app.config.globalProperties)
 
+app.use(createPinia())
 app.mount('#app')
+
+console.log('nw-flavor', process.versions['nw-flavor'])
